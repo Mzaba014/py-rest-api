fffa82fe0a6a3af21c32b4f36c02bf9ffa6a1827
Manuel Zabala <37668464+Mzaba014@users.noreply.github.com>
2021-08-26 14:04:24 -0600

    Implemented User model

--- rest_api/models.py
+++ rest_api/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from datetime import datetime
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for Employees"""
+
+    def create_employee(self, email, name, password=None):
+        """Creates new employee"""
+        if not email:
+            raise ValueError('The email value must be set')
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            name=name
+        )
+
+        user.set_password(password)  # encrypting pass for storage in db
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create new admin user"""
+        user = self.create_employee(
+            email,
+            name,
+            password
+        )
+        user.is_admin = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Database model for employees"""
+
+    emp_id = models.AutoField(_('employee id'), primary_key=True)
+    email = models.EmailField(_('email'), max_length=255, unique=True)
+    first_name = models.CharField(_('first name'), max_length=64)
+    last_name = models.CharField(_('last name'), max_length=64)
+    hire_date = models.DateField(_("hire date"), default=datetime.date.today)
+    birth_date = models.DateField(_('birth date'))
+    is_active = models.BooleanField(_('active'), default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    # get_full_name and get_short_name must be implemented when inheriting from AbstractBaseUser
+    def get_full_name(self):
+        """Returns employee's full name"""
+        return f'{self.first_name} {self.last_name}'
+
+    def get_short_name(self):
+        """Returns employee's full name"""
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        """Return string representation of employee"""
+        return self.email
--- rest_project/settings.py
+++ rest_project/settings.py
@@ -15,18 +15,16 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'br@oi6ii9)tnbitbog89$1ghm4uhbgzz3$ngroo*hm1s_kx8q^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'rest_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -82,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -102,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -116,8 +111,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'rest_api.User'
